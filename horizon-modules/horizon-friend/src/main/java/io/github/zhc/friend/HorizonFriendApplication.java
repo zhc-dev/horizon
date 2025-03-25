8e4bd9d61705e17d2f9484078a0b56238ffd2864
zhc-dev <zhc.1118.dev@gmail.com>
2025-03-25 21:40:20 +0800

    初始化后端工程

---
+++ horizon-modules/horizon-friend/src/main/java/io/github/zhc/friend/HorizonFriendApplication.java
@@ -0,0 +1,15 @@
+package io.github.zhc.friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhc.dev
+ * @date 2025/3/25 20:59
+ */
+@SpringBootApplication
+public class HorizonFriendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HorizonFriendApplication.class, args);
+    }
+}
---
+++ horizon-modules/horizon-system/src/main/java/io/github/zhc/system/HorizonSystemApplication.java
@@ -0,0 +1,15 @@
+package io.github.zhc.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhc.dev
+ * @date 2025/3/25 21:01
+ */
+@SpringBootApplication
+public class HorizonSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HorizonSystemApplication.class, args);
+    }
+}
