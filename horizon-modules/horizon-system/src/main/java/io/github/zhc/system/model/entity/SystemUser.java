d3c5f7f46cb50846363a34f0761d2c60b9dd2996
zhc-dev <zhc.1118.dev@gmail.com>
2025-03-25 22:44:37 +0800

    新增系统用户表

---
+++ horizon-modules/horizon-system/src/main/java/io/github/zhc/system/model/entity/SystemUser.java
@@ -0,0 +1,5 @@
+package io.github.zhc.system.model.entity;/**
+ * @author zhc.dev
+ * @date 2025/3/25 22:35
+ */public class SystemUser {
+}
---
+++ sql/tb.sql
@@ -0,0 +1,16 @@
+# 系统用户表（管理员）
+DROP TABLE IF EXISTS `tb_system_user`;
+CREATE TABLE `tb_system_user`
+(
+    `user_id`       BIGINT(20) unsigned NOT NULL COMMENT '⽤⼾id',
+    `user_account`  VARCHAR(32)  DEFAULT NULL COMMENT '⽤⼾账号',
+    `user_password` VARCHAR(100) DEFAULT NULL COMMENT '⽤⼾密码',
+    `nick_name`     VARCHAR(32)  DEFAULT NULL COMMENT '昵称',
+    `create_by`     BIGINT(8)           NOT NULL COMMENT '创建⽤⼾',
+    `create_time`   DATETIME            NOT NULL COMMENT '创建时间',
+    `update_by`     BIGINT(8)    DEFAULT NULL COMMENT '更新⽤⼾',
+    `update_time`   DATETIME     DEFAULT NULL COMMENT '更新时间',
+    PRIMARY KEY (`user_id`),
+    UNIQUE KEY `user_account` (`user_account`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4 COMMENT ='管理端⽤⼾表'
