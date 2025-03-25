d7efc663106711f6397ece2181fc2fba8d29af54
zhc-dev <zhc.1118.dev@gmail.com>
2025-03-25 21:53:21 +0800

    初始化数据库

--- sql/init.sql
+++ sql/init.sql
@@ -4,3 +4,18 @@ CREATE DATABASE IF NOT EXISTS `horizon_dev`;
 CREATE USER `horizon`@`%` IDENTIFIED BY 'horizon';
 # 赋予用户操作 `horizon_dev` 数据库的权限
 GRANT CREATE, DROP, SELECT, INSERT, UPDATE, DELETE ON horizon_dev.* TO `horizon`@`%`;
+# 选中数据库
+USE horizon_dev;
+# 创建测试表
+CREATE TABLE `tb_test`
+(
+    `test_id` BIGINT UNSIGNED NOT NULL,
+    `title`   TEXT            NOT NULL,
+    `content` TEXT            NOT NULL,
+    PRIMARY KEY (`test_id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+INSERT INTO tb_test VALUES (1, 'test', 'test');
+SELECT * FROM tb_test;
+UPDATE tb_test SET title = 'test_update' WHERE test_id = 1;
