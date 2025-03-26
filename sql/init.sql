edceb27f12d61f6d25b18575468be3fbd9df9c97
zhc-dev <zhc.1118.dev@gmail.com>
2025-03-26 22:44:24 +0800

    赋予数据库用户horizon 权限

--- sql/init.sql
+++ sql/init.sql
@@ -3,7 +3,7 @@ CREATE DATABASE IF NOT EXISTS `horizon_dev`;
 # 创建horizon用户
 CREATE USER `horizon`@`%` IDENTIFIED BY 'horizon';
 # 赋予用户操作 `horizon_dev` 数据库的权限
-GRANT CREATE, DROP, SELECT, INSERT, UPDATE, DELETE ON horizon_dev.* TO `horizon`@`%`;
+GRANT ALTER,CREATE, DROP, SELECT, INSERT, UPDATE, DELETE ON horizon_dev.* TO `horizon`@`%`;
 # 选中数据库
 USE horizon_dev;
 # 创建测试表
