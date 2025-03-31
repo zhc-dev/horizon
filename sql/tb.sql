91bf6e54458676fe4c9391e78f3a8c749fae9ec1
zhc-dev <zhc.1118.dev@gmail.com>
2025-04-01 01:33:02 +0800

    竞赛表&竞赛题目表

--- sql/tb.sql
+++ sql/tb.sql
@@ -17,7 +17,7 @@ CREATE TABLE `tb_system_user`
 
 # 题目表
 DROP TABLE IF EXISTS `tb_question`;
-CREATE TABLE tb_question
+CREATE TABLE `tb_question`
 (
     `question_id`   BIGINT UNSIGNED NOT NULL COMMENT '题目id',
     `title`         VARCHAR(50)     NOT NULL COMMENT '题目标题',
@@ -35,3 +35,31 @@ CREATE TABLE tb_question
     UNIQUE KEY `title` (`title`),
     PRIMARY KEY (`question_id`)
 );
+
+CREATE TABLE `tb_contest`
+(
+    contest_id  BIGINT UNSIGNED NOT NULL COMMENT '竞赛id（主键）',
+    title       VARCHAR(50)     NOT NULL COMMENT '竞赛标题',
+    start_time  DATETIME        NOT NULL COMMENT '竞赛开始时间',
+    end_time    DATETIME        NOT NULL COMMENT '竞赛结束时间',
+    status      TINYINT         NOT NULL COMMENT '0' COMMENT '是否发布 0：未发布  1：已发布',
+    create_by   BIGINT UNSIGNED NOT NULL COMMENT '创建人',
+    create_time DATETIME        NOT NULL COMMENT '创建时间',
+    update_by   BIGINT UNSIGNED COMMENT '更新人',
+    update_time DATETIME COMMENT '更新时间',
+    PRIMARY KEY (contest_id)
+);
+
+
+CREATE TABLE `tb_contest_question`
+(
+    contest_question_id BIGINT UNSIGNED NOT NULL COMMENT '竞赛题目关系id（主键）',
+    question_id         BIGINT UNSIGNED NOT NULL COMMENT '题目id（主键）',
+    contest_id          BIGINT UNSIGNED NOT NULL COMMENT '竞赛id（主键）',
+    question_order      INT             NOT NULL COMMENT '题目顺序',
+    create_by           BIGINT UNSIGNED NOT NULL COMMENT '创建人',
+    create_time         DATETIME        NOT NULL COMMENT '创建时间',
+    update_by           BIGINT UNSIGNED COMMENT '更新人',
+    update_time         DATETIME COMMENT '更新时间',
+    PRIMARY KEY (contest_question_id)
+)
