d862f1e3df1200cbced245bb23b8ccb5f3a236ed
zhc-dev <zhc.1118.dev@gmail.com>
2025-03-31 22:34:33 +0800

    QuestionAddRequest

--- horizon-modules/horizon-system/src/main/java/io/github/zhc/dev/system/controller/question/QuestionController.java
+++ horizon-modules/horizon-system/src/main/java/io/github/zhc/dev/system/controller/question/QuestionController.java
@@ -1,6 +1,7 @@
 package io.github.zhc.dev.system.controller.question;
 
 import io.github.zhc.dev.common.core.controller.BaseController;
+import io.github.zhc.dev.common.core.model.entity.R;
 import io.github.zhc.dev.common.core.model.entity.TableData;
 import io.github.zhc.dev.system.model.dto.question.QuestionQueryRequest;
 import io.github.zhc.dev.system.service.question.QuestionService;
@@ -9,6 +10,7 @@ import jakarta.annotation.Resource;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,10 @@ public class QuestionController extends BaseController {
     @GetMapping("/list")
     public TableData list(@Validated @NotNull QuestionQueryRequest questionQueryRequest) {
         return getTableData(questionService.list(questionQueryRequest));
+    }
+
+    @PostMapping("/add")
+    public R<Void> add(@Validated @NotNull QuestionQueryRequest questionQueryRequest) {
+        return toR(questionService.add(questionQueryRequest));
     }
 }
---
+++ horizon-modules/horizon-system/src/main/java/io/github/zhc/dev/system/model/dto/question/QuestionAddRequest.java
@@ -0,0 +1,37 @@
+package io.github.zhc.dev.system.model.dto.question;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * @author zhc.dev
+ * @date 2025/3/31 22:32
+ */
+@Getter
+@Setter
+public class QuestionAddRequest {
+    @Schema(description = "题目标题")
+    private String title;
+
+    @Schema(description = "题目难度")
+    private Integer difficulty;
+
+    @Schema(description = "时间限制")
+    private Long timeLimit;
+
+    @Schema(description = "空间限制")
+    private Long spaceLimit;
+
+    @Schema(description = "题目内容")
+    private String content;
+
+    @Schema(description = "测试用例")
+    private String questionCase;
+
+    @Schema(description = "默认代码块")
+    private String defaultCode;
+
+    @Schema(description = "main函数")
+    private String mainFuc;
+}
