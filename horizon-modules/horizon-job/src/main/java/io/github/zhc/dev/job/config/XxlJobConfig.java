6387c47e7026cb74d534a8411ba7b2f9da1238f2
zhc-dev <zhc.1118.dev@gmail.com>
2025-04-10 22:38:19 +0800

    feat(job): 完成 xxl-job 的配置类实现

---
+++ horizon-modules/horizon-job/src/main/java/io/github/zhc/dev/job/config/XxlJobConfig.java
@@ -0,0 +1,34 @@
+package io.github.zhc.dev.job.config;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author zhc-dev
+ * @data 2025/4/10 22:35
+ */
+@Slf4j
+@Configuration
+public class XxlJobConfig {
+    @Value("${xxl.job.admin.addresses}")
+    private String adminAddresses;
+
+    @Value("${xxl.job.accessToken}")
+    private String accessToken;
+
+    @Value("${xxl.job.executor.appname}")
+    private String appname;
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        log.info(">>>>>>>>>>> xxl-job config init.");
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAppname(appname);
+        xxlJobSpringExecutor.setAccessToken(accessToken);
+        return xxlJobSpringExecutor;
+    }
+}
