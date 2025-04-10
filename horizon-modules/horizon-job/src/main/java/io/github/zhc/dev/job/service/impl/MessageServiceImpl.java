d408a9af0623f7fd5e0db7ff1158262e3103e7b1
zhc-dev <zhc.1118.dev@gmail.com>
2025-04-10 23:31:29 +0800

    fix(job):注册Service Bean

--- horizon-modules/horizon-job/src/main/java/io/github/zhc/dev/job/service/impl/MessageServiceImpl.java
+++ horizon-modules/horizon-job/src/main/java/io/github/zhc/dev/job/service/impl/MessageServiceImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import io.github.zhc.dev.job.mapper.message.MessageMapper;
 import io.github.zhc.dev.job.model.entity.message.Message;
 import io.github.zhc.dev.job.service.MessageService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
  * @author zhc-dev
  * @data 2025/4/10 23:00
  */
+@Service
 public class MessageServiceImpl extends ServiceImpl<MessageMapper, Message> implements MessageService {
     @Override
     public boolean batchInsert(List<Message> messageTextList) {
--- horizon-modules/horizon-job/src/main/java/io/github/zhc/dev/job/service/impl/MessageTextServiceImpl.java
+++ horizon-modules/horizon-job/src/main/java/io/github/zhc/dev/job/service/impl/MessageTextServiceImpl.java
@@ -4,6 +4,7 @@ import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import io.github.zhc.dev.job.mapper.message.MessageTextMapper;
 import io.github.zhc.dev.job.model.entity.message.MessageText;
 import io.github.zhc.dev.job.service.MessageTextService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -11,6 +12,7 @@ import java.util.List;
  * @author zhc-dev
  * @data 2025/4/10 23:01
  */
+@Service
 public class MessageTextServiceImpl extends ServiceImpl<MessageTextMapper, MessageText> implements MessageTextService {
     @Override
     public boolean batchInsert(List<MessageText> messageTextList) {
